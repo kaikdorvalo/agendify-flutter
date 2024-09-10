84312f4fa24d7fab8820ec49d94f0bc7608cebd3
kaikdorvalo <kaikdorvalo@gmail.com>
2024-09-09 22:14:25 -0300

    steup

---
+++ lib/auth/ui/screens/signin_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({super.key})
+
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
+
