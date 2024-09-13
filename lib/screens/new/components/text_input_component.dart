17b5f464217459f5cd670ad96ac3da95fbc7ef6e
Kaik KDS <kaikdorvalo@gmail.com>
2024-09-13 13:31:29 -0300

    feat: created input text

---
+++ lib/screens/new/components/text_input_component.dart
@@ -0,0 +1,85 @@
+import 'package:agendify/shared/models/scheduling_model.dart';
+import 'package:flutter/material.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+
+class TextInput extends StatefulWidget {
+  String label;
+  String? hint;
+  String? labelText;
+  List<MaskTextInputFormatter>? masks;
+  IconData icon;
+  final Function(String) changeValue;
+
+  TextInput({
+    Key? key,
+    required this.label,
+    required this.icon,
+    required this.changeValue,
+    this.hint,
+    this.labelText,
+    this.masks,
+  }) : super(key: key);
+
+  @override
+  _TextInputState createState() => _TextInputState();
+}
+
+class _TextInputState extends State<TextInput> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          widget.label,
+          style: TextStyle(
+            fontSize: 17.0,
+          ),
+        ),
+        const SizedBox(
+          height: 5.0,
+        ),
+        Container(
+          padding: const EdgeInsets.only(
+            top: 5.0,
+            bottom: 5.0,
+            left: 15.0,
+            right: 15.0,
+          ),
+          decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 255, 255, 255),
+              borderRadius: BorderRadius.circular(5.0),
+              border: Border.all(
+                color: const Color.fromARGB(255, 143, 143, 143),
+                width: 1.0,
+              )),
+          child: TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            style: const TextStyle(
+              color: Color.fromARGB(255, 0, 0, 0),
+            ),
+            decoration: InputDecoration(
+              hintText: widget.hint ?? '',
+              hintStyle: const TextStyle(
+                color: Color.fromARGB(255, 158, 158, 158),
+              ),
+              labelText: widget.labelText ?? '',
+              labelStyle: const TextStyle(
+                color: Color.fromARGB(255, 158, 158, 158),
+              ),
+              icon: Icon(
+                widget.icon,
+                color: Color.fromARGB(255, 33, 149, 243),
+              ),
+              border: InputBorder.none,
+            ),
+            inputFormatters: widget.masks ?? [],
+            onChanged: (value) {
+              widget.changeValue(value);
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/screens/new/screens/new_screen.dart
+++ lib/screens/new/screens/new_screen.dart
@@ -1,11 +1,51 @@
+import 'package:agendify/screens/new/components/text_input_component.dart';
 import 'package:agendify/shared/components/navigation_bottom_component.dart';
+import 'package:agendify/shared/models/scheduling_model.dart';
 import 'package:flutter/material.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
 class NewScreen extends StatefulWidget {
   _NewScreenState createState() => _NewScreenState();
 }
 
 class _NewScreenState extends State<NewScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String name = '';
+  String cpf = '';
+  String description = '';
+  DateTime date = DateTime.now();
+
+  void changeName(String value) {
+    setState(() {
+      name = value;
+    });
+
+    print(name);
+  }
+
+  void changeCpf(String value) {
+    setState(() {
+      cpf = value;
+    });
+  }
+
+  void changeDescription(String value) {
+    setState(() {
+      description = value;
+    });
+  }
+
+  void changeDate(DateTime value) {
+    setState(() {
+      date = value;
+    });
+  }
+
+  MaskTextInputFormatter cpfMask = MaskTextInputFormatter(
+      mask: '###.###.###-##',
+      filter: {"#": RegExp(r'[0-9]')},
+      type: MaskAutoCompletionType.lazy);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,6 +70,40 @@ class _NewScreenState extends State<NewScreen> {
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         padding: const EdgeInsets.all(20.0),
+        child: Form(
+            key: _formKey,
+            child: ListView(
+              children: <Widget>[
+                TextInput(
+                  label: 'Nome',
+                  icon: Icons.person,
+                  changeValue: changeName,
+                  labelText: 'Nome',
+                  hint: 'Seu Nome',
+                ),
+                const SizedBox(
+                  height: 15.0,
+                ),
+                TextInput(
+                  label: 'CPF',
+                  icon: Icons.person_search,
+                  changeValue: changeName,
+                  labelText: 'CPF',
+                  hint: '000.000.00-00',
+                  masks: [cpfMask],
+                ),
+                const SizedBox(
+                  height: 15.0,
+                ),
+                TextInput(
+                  label: 'Descrição',
+                  icon: Icons.description,
+                  changeValue: changeDescription,
+                  labelText: 'Descrição',
+                  hint: 'Realizar extração de siso...',
+                ),
+              ],
+            )),
       ),
     );
   }
