170b825f7e375246df4d83d40176aea91e123012
Kaik Dorvalo <kaikdorvalo@gmail.com>
2024-11-05 23:47:08 -0300

    test: add widget, mock and unity tests

--- test/storage_service_test.dart
+++ test/storage_service_test.dart
@@ -12,7 +12,7 @@ import 'package:http/http.dart' as http;
 final class HttpMethodsMock extends Mock implements HttpMethods {}
 
 void main() {
-  test('Use case: Should get all scheduling', () async {
+  test('Use case: Get all scheduling', () async {
     HttpMethodsMock httpMock = HttpMethodsMock();
     StorageService service = StorageService('agenda', httpMock);
 
@@ -44,7 +44,7 @@ void main() {
     expect(res[1].person.nome, "Vitor");
   });
 
-  test("Use case: Should save Scheduling", () async {
+  test("Use case: Save Scheduling", () async {
     HttpMethodsMock httpMock = HttpMethodsMock();
     StorageService service = StorageService('agenda', httpMock);
 
@@ -69,5 +69,86 @@ void main() {
     var res = await service.saveScheduling(scheduling);
 
     expect(res, isNotNull);
+  });
+
+  test("Use case: don't save Scheduling if person name is empty", () async {
+    HttpMethodsMock httpMock = HttpMethodsMock();
+    StorageService service = StorageService('agenda', httpMock);
+
+    Person person = Person('876.618.400-22', '');
+    Scheduling scheduling = Scheduling(person, 'Extrair siso', DateTime.now());
+
+    final json = jsonEncode(
+      {
+        "id": "904c",
+        "name": "Vitor",
+        "cpf": "345.325.567-77",
+        "description": "Realizar extração de siso",
+        "date": "2024-11-04T22:05:11.902"
+      },
+    );
+
+    final response = http.Response(json, 200);
+
+    when(() => httpMock.post(Uri.parse('http://localhost:3000/agenda'),
+        jsonEncode(scheduling.toJson()))).thenAnswer((_) async => response);
+
+    var res = await service.saveScheduling(scheduling);
+
+    expect(res, '');
+  });
+
+  test("Use case: don't save Scheduling if description is empty", () async {
+    HttpMethodsMock httpMock = HttpMethodsMock();
+    StorageService service = StorageService('agenda', httpMock);
+
+    Person person = Person('876.618.400-22', 'Kaik');
+    Scheduling scheduling = Scheduling(person, '', DateTime.now());
+
+    final json = jsonEncode(
+      {
+        "id": "904c",
+        "name": "Vitor",
+        "cpf": "345.325.567-77",
+        "description": "Realizar extração de siso",
+        "date": "2024-11-04T22:05:11.902"
+      },
+    );
+
+    final response = http.Response(json, 200);
+
+    when(() => httpMock.post(Uri.parse('http://localhost:3000/agenda'),
+        jsonEncode(scheduling.toJson()))).thenAnswer((_) async => response);
+
+    var res = await service.saveScheduling(scheduling);
+
+    expect(res, '');
+  });
+
+  test("Use case: don't save Scheduling when is not valid cpf", () async {
+    HttpMethodsMock httpMock = HttpMethodsMock();
+    StorageService service = StorageService('agenda', httpMock);
+
+    Person person = Person('871.618.400-22', 'Kaik');
+    Scheduling scheduling = Scheduling(person, 'Extrair siso', DateTime.now());
+
+    final json = jsonEncode(
+      {
+        "id": "904c",
+        "name": "Vitor",
+        "cpf": "345.321.567-77",
+        "description": "Realizar extração de siso",
+        "date": "2024-11-04T22:05:11.902"
+      },
+    );
+
+    final response = http.Response(json, 200);
+
+    when(() => httpMock.post(Uri.parse('http://localhost:3000/agenda'),
+        jsonEncode(scheduling.toJson()))).thenAnswer((_) async => response);
+
+    var res = await service.saveScheduling(scheduling);
+
+    expect(res, '');
   });
 }
